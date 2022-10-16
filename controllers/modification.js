bf9eff463fc61311be5c474338294185e3c7061c
nandu2212 <nanda22121998@gmail.com>
2022-10-16 09:30:16 +0530

    finalcommit

--- controllers/modification.js
+++ controllers/modification.js
@@ -10,14 +10,18 @@ app.post("/update",(req,res)=>{
 basedb.connect(function(err){
     if(err) throw err
     var sq=`SELECT (data) FROM frejun2 WHERE ID= ? `;
-    basedb.query(sq,[req.body.id],(err,result)=>{
+    basedb.query(sq,[req.body.id],async(err,result)=>{
 if(err) throw err
-var array=result[0].data.split(" ");
-var modifieddata=datawrite(array);
-// var z=`UPDATE frujen2 SET data=$modifieddata WHERE ID=? `
-// basedb.query(z,[req.body.id],(result,err)=>{
-//     if(err) throw err
-//     res.status(203).send()
+var array=await result[0].data.split(" ");
+var modifieddata=await datawrite(array);
+var z=`UPDATE frejun2 SET DATA="${modifieddata}" WHERE ?`
+var value={
+    ID:req.body.id
+}
+basedb.query(z,value,(err,ddata)=>{
+if(err) throw err;
+res.status(200).send(ddata)
+})
 res.status(204).send(modifieddata)
 })
 
