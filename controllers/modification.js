e2143ab2304d1224a303195c4f0f7b7b5ed9e5d1
nandu2212 <nanda22121998@gmail.com>
2022-10-16 09:39:06 +0530

    MODIFYING DATABASE

--- controllers/modification.js
+++ controllers/modification.js
@@ -9,10 +9,10 @@ const datawrite=require("../utilitys/updatingdb");
 app.post("/update",(req,res)=>{
 basedb.connect(function(err){
     if(err) throw err
-    var sq=`SELECT (data) FROM frejun2 WHERE ID= ? `;
+    var sq=`SELECT (DATA) FROM frejun2 WHERE ID= ? `;
     basedb.query(sq,[req.body.id],async(err,result)=>{
 if(err) throw err
-var array=await result[0].data.split(" ");
+var array=await result[0].DATA.split(" ");
 var modifieddata=await datawrite(array);
 var z=`UPDATE frejun2 SET DATA="${modifieddata}" WHERE ?`
 var value={
