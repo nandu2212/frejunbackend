175b9aa8b4db0f47e1198abaa27d2d0b095a9323
nandu2212 <nanda22121998@gmail.com>
2022-10-16 10:19:57 +0530

    ...

---
+++ utilitys/pagination.js
@@ -0,0 +1,9 @@
+const pagination = async (allData, page, limit) => {
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+  
+    const result = await allData.slice(startIndex, endIndex)
+    return result
+  }
+  
+  module.exports =  pagination ;
